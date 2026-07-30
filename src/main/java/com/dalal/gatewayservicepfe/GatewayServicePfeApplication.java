a604309ec697ec2e1a0a18ec620ebce3038e5ed5
DalalYouness <155886084+DalalYouness@users.noreply.github.com>
2026-07-30 22:04:51 +0100

    routers config

--- src/main/java/com/dalal/gatewayservicepfe/GatewayServicePfeApplication.java
+++ src/main/java/com/dalal/gatewayservicepfe/GatewayServicePfeApplication.java
@@ -2,8 +2,10 @@ package com.dalal.gatewayservicepfe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayServicePfeApplication {
 
     public static void main(String[] args) {
